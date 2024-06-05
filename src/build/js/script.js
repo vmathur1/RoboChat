80a1514741b3b62ce683acfb46856c178a839a1b
Vishal Mathur <37118014+vmathur1@users.noreply.github.com>
2024-06-05 12:32:28 -0700

    Update script.js

--- src/build/js/script.js
+++ src/build/js/script.js
@@ -13,7 +13,7 @@ sendButton.addEventListener("click", async () => {
         const response = await fetch("https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium", {
             method: "POST",
             headers: {
-                "Authorization": "Bearer YOUR_HUGGINGFACE_API_KEY",
+                "Authorization": "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",
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({ inputs: message }),
